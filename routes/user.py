71118d588812550139241af38a3967a9168b5ba9
ThunderGer <Jaames11@hotmail.com>
2022-03-08 03:55:26 -0600

    connect to MondoDB

--- routes/user.py
+++ routes/user.py
@@ -1,14 +1,21 @@
 from fastapi import APIRouter
-
+from config.db import conn
+from schemas.user import userEntity, usersEntity
+from models.user import User
 user = APIRouter()
 
 @user.get('/users')
 def get_users():
-    return "Hola ThunderGer"
+    return usersEntity(conn.local.user.find())
 
 @user.post('/users')
-def create_user():
-    return "Usuario creado"
+def create_user(user: User):
+    new_user = dict(user)
+    del new_user['id']
+    id = conn.local.user.insert_one(new_user).inserted_id
+    
+    user = conn.local.user.find_one({"_id": id})    
+    return userEntity(user)    
 
 @user.get('/users/{id}')
 def find_user():
--- schemas/user.py
+++ schemas/user.py
@@ -1,7 +1,9 @@
-def userEntity(item) -> dict:
+def userEntity(item) -> dict:    
     return {
-        "id": item.get("id"),
+        "id": str(item["_id"]),
         "name": item.get("name"),
         "email": item.get("email"),
         "password": item.get("password")
     }
+def usersEntity(entity) -> list:
+    return [userEntity(item) for item in entity]
